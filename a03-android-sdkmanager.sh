f58ac67f0124fd381031f1acb175d226e32d1bda
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-10 13:34:06 -0700

    Update a03-android-sdkmanager.sh

--- a03-android-sdkmanager.sh
+++ a03-android-sdkmanager.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+
+# just finding the files
+
+cd ./home/gitpod
+
+ls -la
+
+
 # Need this command to run the sdkmanager to install other android platforms
 
-cd /workspace/my-gitpod-ionic-cordova
+#cd /workspace/my-gitpod-ionic-cordova
 
-./android/tools/bin/sdkmanager --list
+#./android/tools/bin/sdkmanager --list
 
 
 # The command to install Android Plafrom 26 would look something like this
 
-yes | ./android/tools/bin/sdkmanager "build-tools;26.0.3" "platforms;android-26" 
+#yes | ./android/tools/bin/sdkmanager "build-tools;26.0.3" "platforms;android-26" 
