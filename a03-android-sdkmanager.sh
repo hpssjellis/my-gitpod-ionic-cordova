6f13664da2aef6c6fcfa70dcd0bce25dcdaca219
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-10 22:22:26 -0700

    Update a03-android-sdkmanager.sh

--- a03-android-sdkmanager.sh
+++ a03-android-sdkmanager.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 
-# just finding the files
-#cd /home/gitpod/.android
-
-#ls -la
-
-echo "Let us run the sdkmanager"
 
 sdkmanager --list
 
-echo "try"
-echo "yes | sdkmanager "build-tools;26.0.3" "platforms;android-26"
+echo ""
+echo "The next line shows how to install android platform 26"
 
-# commented out how to install platform 26
-
-#yes | ./home/gitpod/.android/tools/bin/sdkmanager "build-tools;26.0.3" "platforms;android-26"
-
+echo '"yes | sdkmanager "build-tools;26.0.3" "platforms;android-26"'
 
