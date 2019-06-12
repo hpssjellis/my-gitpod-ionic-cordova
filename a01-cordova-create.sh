2d283aabbb5dd498fb8870b66e057efe7508a564
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-11 19:27:13 -0700

    Update a01-cordova-create.sh

--- a01-cordova-create.sh
+++ a01-cordova-create.sh
@@ -14,16 +14,16 @@ cordova platform add android
 
 cordova build android --no-telemetry
 
+number=$RANDOM
 
-
-cp /workspace/my-gitpod-ionic-cordova/$wow4/platforms/android/app/build/outputs/apk/debug/app-debug.apk /workspace/my-gitpod-ionic-cordova/public/$wow4-app-debug.apk
+cp /workspace/my-gitpod-ionic-cordova/$wow4/platforms/android/app/build/outputs/apk/debug/app-debug.apk /workspace/my-gitpod-ionic-cordova/public/$wow4-app-debug$number.apk
 
 
 
 qrcode -o /workspace/my-gitpod-ionic-cordova/public/qrout.png $(gp url 8001);
-echo "<br><img src='qrout.png'><br>" >> /workspace/my-gitpod-ionic-cordova/public/index.html
+echo "<br>Just a QR code to the same webpage <br><img src='qrout.png'><br>" >> /workspace/my-gitpod-ionic-cordova/public/index.html
 
-echo "<li><a href="$wow4-app-debug.apk">$wow4-app-debug.apk</a><br>" >> /workspace/my-gitpod-ionic-cordova/public/index.html
+echo "<li><a href="$wow4-app-debug$number.apk">$wow4-app-debug$number.apk</a><br>" >> /workspace/my-gitpod-ionic-cordova/public/index.html
 
 
 #open the preview pane
