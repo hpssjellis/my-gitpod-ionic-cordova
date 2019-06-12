102794b5b8f766d577a687340d39c8096cf334a2
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-12 14:39:01 -0700

    Update a06-capacitor-create.sh

--- a06-capacitor-create.sh
+++ a06-capacitor-create.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 
-npx @capacitor/cli create
-npx cap open android
-npx cap serve
 
+echo "The App must have been built with a04-ionic-start.sh"
+echo "Enter a folder name of the app you wish to re-build"
+read wow4
+
+
+
+
+
+npx @capacitor/cli create --npm-client npm  $wow4 $wow4 com.$wow4.$wow4 $wow4
+
+
+
+
+
+cd $wow4
+
+
+npx cap add android
+#npx cap open android
+npx cap serve
 
 #echo "Not sure how this works"
